668b298a9334f8bb718ffce71e4ffa7a37f1c0ed
MV <mattveit1@gmail.com>
2018-09-04 10:40:40 -0500

    Add edit page.

--- Opencare/Pages/Users/Edit.cshtml.cs
+++ Opencare/Pages/Users/Edit.cshtml.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Opencare.Authorization;
 using Opencare.Data;
 using Opencare.Pages.Students;
 
@@ -31,6 +33,57 @@ namespace Opencare.Pages.Users
                                              m => m.Id == id);
 
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // Fetch Contact from DB to get OwnerID.
+            //var user = await Context
+            //    .ApplicationUsers.AsNoTracking()
+            //    .FirstOrDefaultAsync(m => m.Id == id);
+
+            //if (user == null)
+            //{
+            //    return NotFound();
+            //}
+
+            //var isAuthorized = await AuthorizationService.AuthorizeAsync(
+            //                                         User, user,
+            //                                         StudentOperations.Update);
+            //if (!isAuthorized.Succeeded)
+            //{
+            //    return new ChallengeResult();
+            //}
+
+            Context.Attach(AppUser).State = EntityState.Modified;
+
+            try
+            {
+                await Context.SaveChangesAsync();
+            }
+            catch
+            {
+                if (!Context.ApplicationUsers.Any(e => e.Id == AppUser.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool StudentExists(int id)
+        {
+            return Context.Student.Any(e => e.StudentId == id);
         }
     }
 }
