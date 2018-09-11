be1af522232e990aba4207332acc2b01b416ccae
veitm1 <veitm1@AWDT005>
2018-09-11 10:02:16 -0500

    Customize pages.

--- Opencare/Pages/Contact.cshtml.cs
+++ Opencare/Pages/Contact.cshtml.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Opencare.Pages
 {
+    [AllowAnonymous]
     public class ContactModel : PageModel
     {
         public string Message { get; set; }
