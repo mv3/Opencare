d821baf0ebf62e5ee41af69091ca989e0c955bd8
veitm1 <veitm1@AWDT005>
2019-05-21 15:17:27 -0500

    Add back link to Bottles page.

--- Opencare/Migrations/20190521181456_updatebottlemodel.Designer.cs
+++ Opencare/Migrations/20190521181456_updatebottlemodel.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Opencare.Data;
 
 namespace Opencare.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190521181456_updatebottlemodel")]
+    partial class updatebottlemodel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
